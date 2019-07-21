ac690ef8b892df43a1be387d7fe13abda9e1d4ff
Darshan <darshan.murugeppa@smu.ca>
2019-07-21 10:13:46 -0300

    Assignment 2

---
+++ MCDA5550_Assignment2/MCDA5550_Assignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MCDA5550_Assignment2
+//
+//  Created by MSc CDA on 2019-07-21.
+//  Copyright © 2019 MSc CDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
